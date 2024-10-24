451f3e95d1491a68ac08f5e5755e7d06e46b6e61
Tyler Funk <tfunk@webfirst.com>
2024-10-24 15:08:20 -0500

    Change vitepress base to /drupalstudyguide

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   title: 'Drupal Study Guide',
   description:
     'Acquia Drupal certification study guide',
-  base: '/studyguide',
+  base: '/drupalstudyguide',
   srcDir: './docs',
   outDir: './dist',
   cleanUrls: true,
